8320ca811e90a9ac77b43511ea317d80fbe4568d
Christian Murphy <cmurphy@unicon.net>
2017-05-21 18:50:51 -0700

    Build with soffit renderer

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,8 @@ buildscript {
 	val springBootVersion = "2.0.0.BUILD-SNAPSHOT"
 	repositories {
 		mavenCentral()
+		mavenLocal()
+		gradleScriptKotlin()
 		maven { url = uri("https://repo.spring.io/snapshot") }
 		maven { url = uri("https://repo.spring.io/milestone") }
 	}
@@ -18,6 +20,7 @@ buildscript {
 apply {
 	plugin("kotlin")
 	plugin("kotlin-spring")
+	plugin("war")
 	plugin("org.springframework.boot")
 	plugin("io.spring.dependency-management")
 }
@@ -30,15 +33,23 @@ tasks.withType<KotlinCompile> {
 
 repositories {
 	mavenCentral()
+	mavenLocal()
+	gradleScriptKotlin()
 	maven { url = uri("https://repo.spring.io/snapshot") }
 	maven { url = uri("https://repo.spring.io/milestone") }
 }
 
 val kotlinVersion = "1.1.2"
+val uPortalVersion = "5.0.0-SNAPSHOT"
 
 dependencies {
 	compile("org.springframework.boot:spring-boot-starter-web")
 	compile("org.jetbrains.kotlin:kotlin-stdlib-jre8:$kotlinVersion")
 	compile("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
+	compile("org.jasig.portal:uPortal-soffit-renderer:$uPortalVersion")
+
 	testCompile("org.springframework.boot:spring-boot-starter-test")
+
+	"providedRuntime"("org.apache.tomcat.embed:tomcat-embed-jasper")
+	"providedRuntime"("org.springframework.boot:spring-boot-starter-tomcat")
 }
--- src/main/kotlin/org/apereo/example/demo/DemoApplication.kt
+++ src/main/kotlin/org/apereo/example/demo/DemoApplication.kt
@@ -2,8 +2,10 @@ package org.apereo.example.demo
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.apereo.portal.soffit.renderer.SoffitApplication
 
 @SpringBootApplication
+@SoffitApplication
 class DemoApplication
 
 fun main(args: Array<String>) {
